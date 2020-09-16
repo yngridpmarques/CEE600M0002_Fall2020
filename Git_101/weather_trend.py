2700a5adca434e86ce289d7ca49980ea664785f7
yngridpmarques <ypereira@syr.edu>
2020-09-16 16:19:54 -0400

    Results for TMAX

--- Git_101/weather_trend.py
+++ Git_101/weather_trend.py
@@ -14,7 +14,7 @@ weather_dat=pd.read_csv("2281205.csv")
 X = np.array(range(weather_dat.shape[0]))
 
 #extract variable that we want to "predict"
-Y = np.array(weather_dat.TAVG)
+Y = np.array(weather_dat.TMAX)
 
 #THIS LINE IS NONSENSE...or is it? you decide if it should stay or go
 
