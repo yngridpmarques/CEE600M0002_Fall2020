0ed93363bce34d8e1dec709150d3de55351e0b98
LizCarter492 <ekcarter@syr.edu>
2020-09-24 11:31:57 -0400

    Removed directory 6_1_datatypes

--- filelist.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-ftp_dir = "ftp://ftp2.psl.noaa.gov/Datasets/ncep.reanalysis.dailyavgs/surface_gauss/"
-variable = "air.2m.gauss."
-file_type = ".nc"
-time =pd.Series(list(range(1950,1970)))
-time = time.apply(str)
-filepaths= ftp_dir + variable + time + file_type
-filepaths.to_csv("../data/filelist.txt", index=False)
